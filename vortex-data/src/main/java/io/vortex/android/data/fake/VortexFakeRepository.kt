b2f1d3193751e15dba6bc8ef7fd548b8a607a233
Yazan Tarifi <yazantarifi98@gmail.com>
2020-01-20 11:00:18 +0000

    add json response and fake api repository

---
+++ vortex-data/src/main/java/io/vortex/android/data/fake/VortexFakeRepository.kt
@@ -0,0 +1,36 @@
+package io.vortex.android.data.fake
+
+import android.content.res.Resources
+import androidx.annotation.RawRes
+import io.reactivex.Flowable
+import io.reactivex.Observable
+import io.reactivex.Single
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+abstract class VortexFakeRepository<Api> {
+
+    protected val resourceReader: VortexResourceReader by lazy {
+        VortexResourceReader()
+    }
+
+    protected suspend fun getObservableFromJson(res: Resources, @RawRes jsonId: Int):Observable<String> {
+        return withContext(Dispatchers.IO) {
+            Observable.just(resourceReader.readJsonFromResource(res, jsonId))
+        }
+    }
+
+    protected suspend fun getFlowableFromJson(res: Resources, @RawRes jsonId: Int):Flowable<String> {
+        return withContext(Dispatchers.IO) {
+            Flowable.just(resourceReader.readJsonFromResource(res, jsonId))
+        }
+    }
+
+    protected suspend fun getSingleFromJson(res: Resources, @RawRes jsonId: Int):Single<String> {
+        return withContext(Dispatchers.IO) {
+            Single.just(resourceReader.readJsonFromResource(res, jsonId))
+        }
+    }
+
+    abstract fun getService(): Api
+}
