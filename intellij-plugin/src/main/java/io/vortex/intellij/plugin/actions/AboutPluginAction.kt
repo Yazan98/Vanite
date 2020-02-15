387d078d73e4b3d4da8382d98665492b0fcce787
Yazan Tarifi <yazantarifi98@gmail.com>
2020-02-15 08:01:18 +0000

    finish configuration and start actions

---
+++ intellij-plugin/src/main/java/io/vortex/intellij/plugin/actions/AboutPluginAction.kt
@@ -0,0 +1,10 @@
+package io.vortex.intellij.plugin.actions
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class AboutPluginAction : AnAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+
+    }
+}
