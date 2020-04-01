328b3d1f1246b3b163234288e3dc38b157c604be
Yazan Tarifi <yazantarifi98@gmail.com>
2020-04-01 03:09:10 +0100

    add vorex logger module

--- vortex-ui/src/main/java/io/vortex/android/ui/fragment/VortexFragmentReadyState.kt
+++ vortex-ui/src/main/java/io/vortex/android/ui/fragment/VortexFragmentReadyState.kt
@@ -1,10 +1,28 @@
 package io.vortex.android.ui.fragment
 
+import android.os.Bundle
+import android.view.View
+import androidx.annotation.CallSuper
 import androidx.annotation.LayoutRes
+import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
+import io.vortex.android.keys.VortexViewModelState
 import io.vortex.android.reducer.VortexViewModelReadyState
+import kotlinx.coroutines.launch
 
 abstract class VortexFragmentReadyState<VM: VortexViewModelReadyState>(@LayoutRes private val layoutRes: Int) : VortexBaseFragment() {
 
+    @CallSuper
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        getController().getStateHandler().observe(viewLifecycleOwner, Observer {
+            lifecycleScope.launch {
+                onStateChanged(it)
+            }
+        })
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    protected abstract suspend fun onStateChanged(newState: VortexViewModelState)
     protected abstract fun getController(): VM
     override fun getLayoutRes(): Int {
         return layoutRes
